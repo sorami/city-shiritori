3fb1c452ace823d8d9d0d6fe50cb23a8d4f94cbc
Sorami Hisamoto <hi@sorami.dev>
2023-07-16 20:39:00 +0900

    d3, world tour

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,72 @@
-// place files you want to import through the `$lib` alias in this folder.
+// cf. https://observablehq.com/@d3/world-tour
+class Versor {
+	static fromAngles([l, p, g]: [number, number, number]): [number, number, number, number] {
+		l *= Math.PI / 360;
+		p *= Math.PI / 360;
+		g *= Math.PI / 360;
+		const sl = Math.sin(l),
+			cl = Math.cos(l);
+		const sp = Math.sin(p),
+			cp = Math.cos(p);
+		const sg = Math.sin(g),
+			cg = Math.cos(g);
+		return [
+			cl * cp * cg + sl * sp * sg,
+			sl * cp * cg - cl * sp * sg,
+			cl * sp * cg + sl * cp * sg,
+			cl * cp * sg - sl * sp * cg
+		];
+	}
+	static toAngles([a, b, c, d]: [number, number, number, number]): [number, number, number] {
+		return [
+			(Math.atan2(2 * (a * b + c * d), 1 - 2 * (b * b + c * c)) * 180) / Math.PI,
+			(Math.asin(Math.max(-1, Math.min(1, 2 * (a * c - d * b)))) * 180) / Math.PI,
+			(Math.atan2(2 * (a * d + b * c), 1 - 2 * (c * c + d * d)) * 180) / Math.PI
+		];
+	}
+	static interpolateAngles(a: [number, number, number], b: [number, number, number]) {
+		const i = Versor.interpolate(Versor.fromAngles(a), Versor.fromAngles(b));
+		return (t: number) => Versor.toAngles(i(t));
+	}
+	static interpolateLinear(
+		[a1, b1, c1, d1]: [number, number, number, number],
+		[a2, b2, c2, d2]: [number, number, number, number]
+	) {
+		(a2 -= a1), (b2 -= b1), (c2 -= c1), (d2 -= d1);
+		const x = new Array(4);
+		return (t: number) => {
+			const l = Math.hypot(
+				(x[0] = a1 + a2 * t),
+				(x[1] = b1 + b2 * t),
+				(x[2] = c1 + c2 * t),
+				(x[3] = d1 + d2 * t)
+			);
+			(x[0] /= l), (x[1] /= l), (x[2] /= l), (x[3] /= l);
+			return x;
+		};
+	}
+	static interpolate(
+		[a1, b1, c1, d1]: [number, number, number, number],
+		[a2, b2, c2, d2]: [number, number, number, number]
+	) {
+		let dot = a1 * a2 + b1 * b2 + c1 * c2 + d1 * d2;
+		if (dot < 0) (a2 = -a2), (b2 = -b2), (c2 = -c2), (d2 = -d2), (dot = -dot);
+		if (dot > 0.9995) return Versor.interpolateLinear([a1, b1, c1, d1], [a2, b2, c2, d2]);
+		const theta0 = Math.acos(Math.max(-1, Math.min(1, dot)));
+		const x = new Array(4);
+		const l = Math.hypot((a2 -= a1 * dot), (b2 -= b1 * dot), (c2 -= c1 * dot), (d2 -= d1 * dot));
+		(a2 /= l), (b2 /= l), (c2 /= l), (d2 /= l);
+		return (t: number) => {
+			const theta = theta0 * t;
+			const s = Math.sin(theta);
+			const c = Math.cos(theta);
+			x[0] = a1 * c + a2 * s;
+			x[1] = b1 * c + b2 * s;
+			x[2] = c1 * c + c2 * s;
+			x[3] = d1 * c + d2 * s;
+			return x;
+		};
+	}
+}
+
+export { Versor };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,109 @@
 <script lang="ts">
 	import 'virtual:uno.css';
+	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
+	import * as topojson from 'topojson-client';
+	import type { Topology } from 'topojson-specification';
+	import { Versor } from '$lib';
+
+	onMount(async () => {
+		const width = window.innerWidth;
+		const height = window.innerHeight;
+
+		const world: Topology | undefined = await d3.json(
+			'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json'
+		);
+		if (!world) return;
+		const land = topojson.feature(world, world.objects.land);
+		// @ts-ignore
+		const borders = topojson.mesh(world, world.objects.countries, (a, b) => a !== b);
+		// @ts-ignore
+		const countries = topojson.feature(world, world.objects.countries).features;
+		// const sphere = { type: 'Sphere' };
+		const sphere = { type: 'Sphere' };
+		const tilt = 20;
+
+		// cf. https://github.com/observablehq/stdlib/blob/main/src/dom/context2d.js
+		const dpi = window.devicePixelRatio;
+		const canvas = document.createElement('canvas');
+		canvas.width = width * dpi;
+		canvas.height = height * dpi;
+		canvas.style.width = width + 'px';
+		const context = canvas.getContext('2d');
+		if (!context) return;
+		context.scale(dpi, dpi);
+		document.getElementById('wrapper')!.appendChild(canvas);
+
+		const projection = d3.geoOrthographic().fitExtent(
+			[
+				[10, 10],
+				[width - 10, height - 10]
+			],
+			sphere
+		);
+		const path = d3.geoPath(projection, context);
+
+		function render(country, arc) {
+			if (!context) return;
+			context.clearRect(0, 0, width, height);
+			context.beginPath(), path(land), (context.fillStyle = '#ccc'), context.fill();
+			context.beginPath(), path(country), (context.fillStyle = '#f00'), context.fill();
+			context.beginPath(),
+				path(borders),
+				(context.strokeStyle = '#fff'),
+				(context.lineWidth = 0.5),
+				context.stroke();
+
+			context.beginPath(),
+				path(sphere),
+				(context.strokeStyle = '#000'),
+				(context.lineWidth = 1.5),
+				context.stroke();
+
+			context.beginPath(),
+				path(arc),
+				(context.strokeStyle = '#333'),
+				(context.lineWidth = 3),
+				context.stroke();
+
+			return context.canvas;
+		}
+
+		let p1: [number, number];
+		let p2: [number, number] = [0, 0];
+		let r1: [number, number, number];
+		let r2: [number, number, number] = [0, 0, 0];
+		for (const country of countries) {
+			const name = country.properties.name;
+
+			p1 = p2;
+			p2 = d3.geoCentroid(country);
+			console.log({ p2 });
+			r1 = r2;
+			r2 = [-p2[0], tilt - p2[1], 0];
+			const ip = d3.geoInterpolate(p1, p2);
+			const iv = Versor.interpolateAngles(r1, r2);
+
+			await d3
+				.transition()
+				.duration(1500)
+				.tween('render', () => (t) => {
+					projection.rotate(iv(t));
+					render(country, { type: 'LineString', coordinates: [p1, ip(t)] });
+				})
+				.transition()
+				.tween('render', () => (t) => {
+					render(country, { type: 'LineString', coordinates: [ip(t), p2] });
+				})
+				.end();
+		}
+	});
 </script>
 
 <svelte:head>
 	<title>都市名しりとり</title>
 </svelte:head>
 
-<div class="grid h-screen place-items-center">body</div>
+<div class="grid h-screen place-items-center">
+	<div id="wrapper" />
+</div>
