329f7f1d7595b16e966e192b90dfcfacb8ecb264
Sorami Hisamoto <hi@sorami.dev>
2023-07-17 14:51:22 +0900

    char boxes

--- src/routes/Name.svelte
+++ src/routes/Name.svelte
@@ -20,20 +20,33 @@
 		<div
 			in:fly={{ x: xShift, duration: transitionDuration }}
 			out:fly={{ x: -xShift, duration: transitionDuration }}
-			class="z-10 absolute bg-black rounded-xl text-white w-xs px-1 py-5 text-center opacity-70"
+			class="z-10 absolute bg-neutral-9 rounded text-white w-sm px-3 py-4 text-center opacity-70"
 		>
-			<div class="flex flex-col gap-y-2">
-				<div class="text-xs tracking-widest italic">
-					<span class="font-bold">{city.shiritori.first}</span> → 「{city.reading}」 →
-					<span class="font-bold">{city.shiritori.last}</span>
+			<div class="flex justify-between items-center">
+				<div
+					class="text-lg font-bold bg-white/70 text-black p-2 w-4 h-4 flex justify-center items-center"
+				>
+					{city.shiritori.first}
 				</div>
-				<div class="text-xl font-bold">{city.name}</div>
+				<div class="font-bold">→</div>
+				<div class="flex flex-col gap-y-2">
+					<div class="text-xs tracking-widest italic">
+						{city.reading}
+					</div>
+					<div class="text-xl font-bold">{city.name}</div>
 
-				<div class="text-xs">
-					{city.country}
+					<div class="text-xs">
+						{city.country}
+					</div>
+					<div class="text-xs">
+						{(city.population / 10000).toFixed(0)}万人
+					</div>
 				</div>
-				<div class="text-xs">
-					{(city.population / 10000).toFixed(0)}万人
+				<div>→</div>
+				<div
+					class="text-lg font-bold bg-white/70 text-black p-2 w-4 h-4 flex justify-center items-center"
+				>
+					{city.shiritori.last}
 				</div>
 			</div>
 		</div>
